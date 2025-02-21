19e4a521c808d848a75d2dfbace21d16ade3dc04
azim-mia <mdazimmia01980@gmail.com>
2025-02-21 21:21:53 +0600

    add code

---
+++ src/lips/findSingle.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+const findSingle = async(url, method)=>{
+  try{
+    const {data} =await axios({
+  method: method, //you can set what request you want to be
+ withCredentials: true,
+  url: url,
+  headers: {
+    Authorization: ''
+  }
+})
+return {data};
+  }catch(error:any){
+    return {error}
+  }
+}
+export default findSingle;
