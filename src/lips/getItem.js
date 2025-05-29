7d31a634fcf517bd24ef2c8dcb8bf45fc73a63c3
azim-mia <mdazimmia01980@gmail.com>
2025-05-29 07:42:42 +0600

    change

---
+++ src/lips/getItem.js
@@ -0,0 +1,19 @@
+const getItem =(url)=>{
+  const res = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Cookie: 'sessionId=your-session-id; userToken=abc123;', // Replace with your actual cookie values
+    },
+    next: {
+      revalidate: 60, // ISR: revalidate every 60 seconds
+    },
+    cache: 'force-cache',
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+const data =await res.json();
+  return data;
+}
+export default getItem;
