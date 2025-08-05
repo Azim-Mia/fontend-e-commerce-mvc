a7aec62302e633d76fac679d921b79802ae42a98
azim <mdazimmia53@gmail.com>
2025-08-05 22:56:25 +0600

    update3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-"use strict";
+/*"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var nextConfig = {
 /* config options here */
-};
-exports.default = nextConfig;
+//};
+//exports.default = nextConfig;
+*/
