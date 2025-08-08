457396cb0f5b7495e33f22d5336a317e8c817bee
azim <mdazimmia53@gmail.com>
2025-08-08 11:55:16 +0600

    update5

--- next.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var nextConfig = {
-    experimental: {
-        serverActions: {}, // খালি object দিয়ে ট্রাই করতে পারো
-    },
-};
-nextConfig; // এটা ফাইলের বাইরে থাকতে হবে, object এর ভিতরে নয়
