909cffac70bbb325873a27efb3a613347bfae01c
azim <mdazimmia53@gmail.com>
2025-08-08 11:35:04 +0600

    update1

--- src/app/api/requestHeaders/route.jsx
+++ src/app/api/requestHeaders/route.jsx
@@ -1,9 +1,8 @@
-'use server';
 import { cookies } from 'next/headers';
 export async function GET(){
   const cookiesRequest =await cookies()
   const session =cookiesRequest.get('sessionId')?.value;
-  console.log(session);
+  console.log("session" + session);
   return Response.json({message:"successfull"},{
     headers:{
       'Content-Type':'application/x-www-form-urlencoded',
@@ -13,7 +12,7 @@ export async function GET(){
 }
 
 
-export default async function POST(request) {
+export async function POST(request) {
   const data = await request.json();
   console.log(data);
   const expire = new Date(Date.now() + 1200 * 1000); //120 seconds from now
