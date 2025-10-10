92d90a46880d073f26cba9cecbfe67ab9d6fe622
azim <mdazimmia53@gmail.com>
2025-10-11 00:22:52 +0600

    change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // এখানে আর কিছু লাগবে না
+  images: {
+    unoptimized: true, // sharp বাদ দেবে
+  },
 }
 
 module.exports = nextConfig;
