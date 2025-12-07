27f2d59a33604b172caf677625861b9bb1416b8f
azim <mdazimmia53@gmail.com>
2025-12-07 13:51:45 +0600

    change

--- src/app/deshboard/orders/update/[orderId]/page.jsx
+++ src/app/deshboard/orders/update/[orderId]/page.jsx
@@ -6,3 +6,19 @@ const OrderUpdate = async({params})=>{
   </>)
 }
 export default OrderUpdate;
+export default DeleteProduct;
+export async function generateStaticParams() {
+  const products = [
+    {
+    orderId:1,
+    name:'Matador',
+  },
+      {
+    orderId:2,
+    name:'Matador',
+  },
+  ]
+  return products.map((data) => ({
+    orderId: data.orderId.toString(),
+  }));
+}
