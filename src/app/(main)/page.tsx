d3011e7a60d0d23cc5d69c4ef29c611ada67d6e2
azim <mdazimmia53@gmail.com>
2026-02-26 01:05:28 +0600

    add modify

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,24 +1,39 @@
-//import Image from "next/image";
+import { Suspense } from "react";
 import Slider from '@/components/Slider'
 import ProductList  from '@/components/ProductList'
 import NewProducts from '@/components/NewProducts'
 import CategoryList from  '@/components/CategoryList'
+
 export default function HomePage() {
-  const api = process.env.SERVER_API;
-  console.log(api)
-  return (<div className="flex p flex-col gap-8" >
-  <div><Slider /></div>
-    <div>
-        <div>
-  <h1 className="text-3xl p-2">Features Products</h1>
-  <ProductList />
-  </div>
-  <h1 className="text-3xl p-2">Category Products</h1>
-  <CategoryList />
-  </div>
-  <div>
-  <h1 className="text-3xl p-2">New Products</h1>
-<NewProducts/>
-  </div>
-  </div>);
+
+  return (
+    <div className="flex flex-col gap-8">
+      
+      <div>
+        <Slider />
+      </div>
+
+      <div>
+        <h1 className="text-3xl p-2">Features Products</h1>
+        <Suspense fallback={<p>Loading Featured Products...</p>}>
+          <ProductList />
+        </Suspense>
+      </div>
+
+      <div>
+        <h1 className="text-3xl p-2">Category Products</h1>
+        <Suspense fallback={<p>Loading Categories...</p>}>
+          <CategoryList />
+        </Suspense>
+      </div>
+
+      <div>
+        <h1 className="text-3xl p-2">New Products</h1>
+        <Suspense fallback={<p>Loading New Products...</p>}>
+          <NewProducts />
+        </Suspense>
+      </div>
+
+    </div>
+  );
 }
