fc8732ae3933afe113f3e16c4c8321008717ea0f
azim <mdazimmia53@gmail.com>
2026-05-01 00:20:58 +0600

    ready for deploy

--- src/app/deshboard/inventories/delete/[inventoryId]/page.tsx
+++ src/app/deshboard/inventories/delete/[inventoryId]/page.tsx
@@ -1,34 +1,9 @@
-
-const url = process.env.NEXT_PUBLIC_URL ?? "";
-
-type Props = {
-  params: {
-    inventoryId: string;
-  };
-};
-
-export function generateStaticParams() {
-  return [
-    { inventoryId: "1" },
-    { inventoryId: "2" },
-    { inventoryId: "3" },
-  ];
+const url = process.env.URL;
+const DeleteInventory = ({params})=>{
+ const inventoryId = params.inventoryId;
+  return (<>
+    DeleteInventory {inventoryId}  
+    {url}
+    </>)
 }
-
-const DeleteInventory = ({ params }: Props) => {
-  const inventoryId = params.inventoryId;
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Delete Inventory</h1>
-
-      <p>ID: {inventoryId}</p>
-      <p>URL: {url}</p>
-
-      <button>
-        Delete
-      </button>
-    </div>
-  );
-};
-
 export default DeleteInventory;
